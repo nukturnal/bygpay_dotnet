3a69440eadd7fa9e5f2e9a7eccec9e873b0d956e
Alfred Rowe <alfred@encodevlabs.com>
2017-06-15 03:43:27 +0000

    SDK Started

---
+++ BygPay/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BygPay
+{
+    public class Configuration
+    {
+		public string BaseUrl { get; set; }
+		public string ApiKey { get; set; }
+
+		public string DepositMobilePath { get; set; }
+		public string DepositStatusOveridePath { get; set; }
+		public string DepositStatusPath { get; set; }
+
+		public string WithdrawMobilePath { get; set; }
+		public string WithdrawStatusOveridePath { get; set; }
+		public string WithdrawStatusPath { get; set; }
+
+        public Configuration()
+        {
+            DepositMobilePath = "/deposits/mobile";
+            DepositStatusOveridePath = "/deposits/status-overide";
+            DepositStatusPath = "/deposits";
+
+            WithdrawMobilePath = "/withdrawals/mobile";
+            WithdrawStatusOveridePath = "/withdrawals/status-overide";
+            WithdrawStatusPath = "/withdrawals";
+        }
+    }
+}
